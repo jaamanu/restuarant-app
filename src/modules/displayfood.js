42f9b1b9ab5780233df7fa80649e242885ee14c1
Azizbek Ruzmatov <azizbekruzmatovraa@gmail.com>
2022-10-29 17:54:44 +0500

    Revert "Add popup-comment.js and footer.js"

--- src/modules/displayfood.js
+++ src/modules/displayfood.js
@@ -1,14 +1,11 @@
-import { foodApiObj, commentUrl } from './APIs.js';
+import { foodApiObj } from './APIs';
 
-import Comment from './comments.js';
-// import { modalDisplay } from './modules/commentpop.js';
-const main = document.querySelector('main');
-const popup = document.querySelector('#modalDisplay');
 const main = document.querySelector('main');
 
 /* eslint-disable */
 export const display = async () => {
 /* eslint-enable */
+  // let markup = '';
 
   foodApiObj.forEach(async (item) => {
     try {
@@ -19,12 +16,6 @@ export const display = async () => {
       }
 
       data.meals.forEach((item) => {
-        const section = document.createElement('section');
-        const div = document.createElement('div');
-        const btn = document.createElement('button');
-        const btn2 = document.createElement('button');
-
-        section.innerHTML = `
         main.innerHTML += `
             <section class="food_items flex">
               <div class="">
@@ -43,108 +34,6 @@ export const display = async () => {
                   </span>
                 </div>
               </div>
-            `;
-        btn.innerText = 'Comments';
-        btn2.innerText = 'Comments';
-        section.classList = 'food_items flex';
-        div.classList = 'column flex';
-        btn.classList = 'button';
-        btn2.classList = 'button';
-        div.append(btn, btn2);
-        section.append(div);
-        main.append(section);
-
-        btn.addEventListener('click', () => {
-          const pop = () => {
-            popup.innerHTML = `
-            <div class="popIt">
-              <div class ="flex pop column">
-               <span class="close-button">&times;</span>
-               <div class="popup_image">
-                  <img class="pop_image" src="${item.strMealThumb}" alt="asdf">
-                </div>
-                <p class="pop_meal">Name: ${item.strMeal}</p> 
-                <p> Location: ${item.strArea}</p>
-                <p class="instructions">${item.strInstructions}<p>
-                <p> Category: ${item.strCategory}</p>
-                <div class="comment_count">
-                </div>
-                <button class="view_more">View Comments</button>
-                <div class="pop_comment">
-                
-                </div>
-                <div id ="commentList"></div>
-                  <h3 class="add-comm">Add a comment</h3>
-                <div class="inputDiv">
-                  <form class="form">
-                    <input id="name"  type="text" name="user" required placeholder="Your Name"><br>
-                    <textarea id="text" type="text" rows=6 name="text" required placeholder="Your insight"></textarea><br>
-                    <div>
-                    <button class="submit-btn" type="submit">Comment</button>
-                  </div>
-                  </form>
-                  
-                </div>
-              </div>
-              </div>`;
-            popup.style.display = 'flex';
-            document.body.style.overflow = 'hidden';
-          };
-          pop();
-          const commentCount = document.querySelector('.comment_count');
-          const commentPop = document.querySelector('.pop_comment');
-
-          const comment = async (username, comment) => {
-            const comments = new Comment(username, comment, item.idMeal);
-            const response = await fetch(commentUrl, { method: 'POST', headers: { 'content-type': 'application/json' }, body: JSON.stringify(comments) });
-            const data = response;
-            return data;
-          };
-          const bisplay = async (gameData) => {
-            commentPop.innerHTML = '';
-            const displayScores = gameData.map((list) => `<div class="new_list">
-                                                              <p> ${list.creation_date} </p>
-                                                              <p> ${list.username} </p>
-                                                              <p> ${list.comment} </p>
-                                                            </div>`).join('');
-            commentPop.innerHTML = displayScores;
-          };
-
-          const get = async (id) => {
-            const response = await fetch(`${commentUrl}?item_id=${id}`);
-            const data = await response.json();
-            if (response.ok) {
-              bisplay(data);
-              commentCount.innerHTML = data.length;
-            }
-          };
-
-          const name = document.querySelector('#name');
-          const text = document.querySelector('#text');
-          const clearInput = () => {
-            name.value = '';
-            text.value = '';
-          };
-
-          const form = document.querySelector('.form');
-          form.addEventListener('submit', (e) => {
-            e.preventDefault();
-            const name = document.querySelector('#name').value;
-            const text = document.querySelector('#text').value;
-            comment(name, text);
-            clearInput();
-          });
-          const close = document.querySelector('.close-button');
-          close.addEventListener('click', () => {
-            get(item.idMeal);
-            popup.style.display = 'none';
-            document.body.style.overflow = 'auto';
-          });
-          const viewMore = document.querySelector('.view_more');
-          viewMore.addEventListener('click', () => {
-            get(item.idMeal);
-          });
-        });
               <div class="column flex">
                 <button class="button">Comments</button>
                 <button class="button">Reservations</button>
